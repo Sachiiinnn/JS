a53f82659f0d10b3658eddd97a75d3a3fad12bf3
Sachiiinnn <sanshu9582@gmail.com>
2025-06-27 13:09:16 +0530

    variables

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 123456;
+let accountEmail = "sanshu9582@gmail.com";
+var accountPassword = "12345678"; 
+accountCity = "Delhi"; // This will create a global variable
+
+accountId = 1;
+console.log(accountId);
