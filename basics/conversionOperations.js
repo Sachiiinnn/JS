06502f68252083a613b659042e7d9b561ce5af62
Sachiiinnn <sanshu9582@gmail.com>
2025-06-27 20:16:11 +0530

    operations

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -21,3 +21,20 @@ console.log(booleanIsLoggedIn); // true
 // 0 => false
 //"" => false
 // "abc" => true
+
+
+// operations
+let value = 3;
+let negValue = -value; // unary operator
+// console.log(negValue); // -3
+// console.log(3 + 3); // 6
+// console.log(3 - 3); // 0
+// console.log(3 * 3); // 9
+// console.log(3 / 3); // 1
+// console.log(3 % 2); // 1 (modulus operator)
+// console.log(3 ** 2); // 9 (exponentiation operator)
+// console.log(3 + "3"); // "33" (string concatenation)
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); // 101
