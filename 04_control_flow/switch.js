1e09b86e3a82479f8b2027d7a8bd74d7cd2e3856
Sachiiinnn <sanshu9582@gmail.com>
2025-06-29 20:35:41 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Jan";
+
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        
+        break;
+    case "Feb":
+        console.log("Feb");
+        
+        break;
+    case "Mar":
+        console.log("Mar");
+        
+        break; // if break no use then it will execute all after condition true except default
+    case "April":
+        console.log("april");
+        
+        break;
+    default:
+        console.log("no month")
+        break;
+}
