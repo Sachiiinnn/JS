4e15a65f275d4a7cdabdc9b3db393c53f8a32a2a
Sachiiinnn <sanshu9582@gmail.com>
2025-06-30 13:13:43 +0530

    map and chaining

--- 05_iterations/chaining.js
+++ 05_iterations/chaining.js
@@ -1,6 +1,6 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
 
-// const newNums = myNums.map((num) => {
+// const newNums = myNums.map((num) => { // map automatically returns
 //     return num + 10;
 // })
 
